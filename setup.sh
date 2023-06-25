7ec9db303b326cf960356261ac408c9a95b8a67e
Bryan Clark <bryan@bclark.net>
2023-06-25 23:05:23 +0000

    adds a setup script for auto installs

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Function to detect the distro
+detect_distro() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        DISTRO=$ID
+    else
+        echo "Cannot identify the distribution."
+        exit 1
+    fi
+}
+
+# Install function for Debian and Ubuntu
+install_debian_ubuntu() {
+    sudo apt-get update
+    sudo apt-get install -y stow
+}
+
+# Install function for Red Hat and Fedora
+install_redhat_fedora() {
+    sudo dnf install -y stow
+}
+
+# Detect the distribution
+detect_distro
+
+# Choose the install function based on the detected distribution
+case "$DISTRO" in
+    "ubuntu"|"debian")
+        install_debian_ubuntu
+        ;;
+    "rhel"|"fedora")
+        install_redhat_fedora
+        ;;
+    *)
+        echo "Unsupported distribution: $DISTRO"
+        exit 1
+        ;;
+esac
+
+echo "GNU Stow installation complete."
+echo "Installing dotfiles"
+stow .
