42af80b7f82ed71ff8f85e2eef49d8569ff6d674
Bryan Clark <bryan@bclark.net>
2024-05-02 16:26:11 -0400

    fix(emacs): replace Hack font in org config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -67,7 +67,7 @@
                   (org-level-6 . 1.1)
                   (org-level-7 . 1.1)
                   (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Hack" :weight 'regular :height (cdr face)))
+    (set-face-attribute (car face) nil :font "FiraCode Nerd Font Mono" :weight 'regular :height (cdr face)))
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
   (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
